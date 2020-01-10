38d32deb0cd777b0e4fa2ed4265636dce570a2eb
Amy Franz <amy.franz@gmail.com>
2020-01-10 16:23:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |item|
     item = "#{item.capitalize!}"
-    item = "#{item}!"
+    item.splice("!")
   end
   puts planeteer_calls
 end
